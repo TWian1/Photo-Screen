b6f3bc7935b143be791258bf82fd195852d8dfd0
Ian <80492876+TWian1@users.noreply.github.com>
2023-01-02 08:09:46 -0500

    Update Screen.py

--- ScreenImport/Screen.py
+++ ScreenImport/Screen.py
@@ -1,19 +1,15 @@
-import math, json, os, numpy as np
-from PIL import Image
-def imgPrint(auto, pixelsarray, w = 10, h = 10, file = ""):
-  if file != "": pixelsarray = np.array(Image.open("./" + file)).tolist()
-  if auto: h,w = len(pixelsarray),len(pixelsarray[0])
-  for a2 in range(math.floor(h/2)):
-    a = a2
-    if h != len(pixelsarray)/2: a = a2 * (len(pixelsarray)/(h*2))
-    if a % ((len(pixelsarray)/h)*2) != 0:
-      temp = ""
-      for b2 in range(w):
-        b,clr = b2,[0,0,0,0,0,0]
-        if w != len(pixelsarray[0]): b = b2 * (len(pixelsarray[0])/w)
-        try:
-          for c in range(3): clr[c] = pixelsarray[math.floor(a)][math.floor(b)][c]
-          for c in range(3): clr[c+3] = pixelsarray[math.floor(a)-1][math.floor(b)][c]
-        except Exception: pass
-        temp += f"\033[38;2;{clr[0]};{clr[1]};{clr[2]}m\033[48;2;{clr[3]};{clr[4]};{clr[5]}m▄\033[0m"
-      print(temp)
+from math import floor
+from numpy import array
+import PIL.Image
+def imgPrint(file,pre=False):
+  pixelsarray =array(PIL.Image.open("./" + file)).tolist()
+  height,width,total = len(pixelsarray),len(pixelsarray[0]),[]
+  for a in range(floor(height/2)):
+    string_temp = ""
+    for b in range(width):
+      clr = [0,0,0,0,0,0]
+      for c in range(6): clr[c] = pixelsarray[floor(a*2)+(floor((5-c)/3))][floor(b)][c%3]
+      string_temp += f"\033[38;2;{clr[0]};{clr[1]};{clr[2]}m\033[48;2;{clr[3]};{clr[4]};{clr[5]}m▄\033[0m"
+    if pre == False: print(string_temp)
+    else: total.append(string_temp)
+  if pre: return total
